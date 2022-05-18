5e9e6acf97c11f5be53517dc239d5709dc26622f
Anton <100033754+Wusele@users.noreply.github.com>
2022-05-18 19:18:33 +0200

    Added Contact Email

--- script.js
+++ script.js
@@ -3,16 +3,6 @@ backButton = document.getElementById("backBtn");
 window.onload = function() {topFunction()};
 window.onscroll = function() {scrollFunction()};
 
-document.getElementById("checkbox").addEventListener('change', (event) => {
-    if(event.currentTarget.checked) {
-        var element = document.body;
-        element.className = "dark-mode";
-    }else{
-        var element = document.body;
-        element.className = "light-mode";
-    }
-})
-
 function scrollFunction() {
     if(document.body.scrollTop > 1600 || document.documentElement.scrollTop > 1600) {
         backButton.style.display = "block";
@@ -21,17 +11,33 @@ function scrollFunction() {
     }
 }
 
+function printScroll() {
+    console.log(document.documentElement.scrollTop);
+}
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
 function linksFunction() {
+    document.body.scrollTop = 1862;
     document.documentElement.scrollTop = 1862;
-    console.log(document.documentElement.scrollTop);
 }
 
 function aboutFunction() {
+    document.body.scrollTop = 941;
     document.documentElement.scrollTop = 941;
-    console.log(document.documentElement.scrollTop);
+}
+
+function contactFunction() {
+    document.body.scrollTop = 1890;
+    document.documentElement.scrollTop = 1890;
+}
+
+
+
+function copyTextToClipboard(text) {
+    navigator.clipboard.writeText(text);
+    console.log('Copied text to clipboard: ' + text)
 }
